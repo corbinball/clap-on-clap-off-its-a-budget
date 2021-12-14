af5e797bbdd4779bee2d3028f7b6126e2e310b1d
Corbin Ball <c.ball@me.com>
2021-12-13 23:10:25 -0600

    added code to install service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,3 +11,24 @@ const FILES_TO_CACHE = [
   "/icons/icon-512x512.png",
 
 ];
+
+
+// install
+self.addEventListener("install", function (evt) {
+    // pre cache image data
+    evt.waitUntil(
+      caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+    );
+      
+    // pre cache all static assets
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+  
+    // tell the browser to activate this service worker immediately once it
+    // has finished installing
+    self.skipWaiting();
+  });
+
+
+  
