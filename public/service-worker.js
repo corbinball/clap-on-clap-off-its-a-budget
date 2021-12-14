44a0656baceba2d29b2f6d6940d139f953b9ab5e
Corbin Ball <c.ball@me.com>
2021-12-13 23:05:07 -0600

    added cache name, data cache name, and files to cache

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "static-cache-money-v2";
+const DATA_CACHE_NAME = "data-cache-datmoney-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/manifest.webmanifest",
+  "/style.css",
+  "/db.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+
+];
