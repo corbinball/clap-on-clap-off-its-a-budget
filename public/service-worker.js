7c1822de5ab050c572ab5bb2245a28fb7c4b31ea
Corbin Ball <c.ball@me.com>
2021-12-15 23:19:12 -0600

    small change

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,9 +15,9 @@ const FILES_TO_CACHE = [
 
 // install
 self.addEventListener("install", function (evt) {
-    evt.waitUntil(
-      caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
-    );
+    // evt.waitUntil(
+    //   caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+    // );
     evt.waitUntil(
       caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
     );
