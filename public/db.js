ba40bffb6840c413a8b8030225602719ef674710
Corbin Ball <c.ball@me.com>
2022-01-17 18:09:26 -0600

    updated index, started on db.js for offline

---
+++ public/db.js
@@ -0,0 +1,21 @@
+let db;
+
+let clapOnOffVersion;
+
+
+const request = indexedDB.open("Clap off Database", 1);
+
+request.onupgradeneeded = function (e) {
+  console.log(request.result.name);
+
+let newVersion = e.newVersion || db.version;
+
+console.log(`Clap on, Clap off updated to ${newVersion}`);
+
+db = e.target.result;
+
+if (db.objectStoreNames.length ===0){
+    db.createObjectStore('ClapMoney', {autoIncrement: true});
+}
+
+};
